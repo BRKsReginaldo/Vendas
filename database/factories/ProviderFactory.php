c509e6a64c5c478106ef8eb7c17904e69e6ffbba
Reginaldo Junior <reginaldo@deltacoding.xyz>
2018-10-22 22:00:24 -0300

    updated ProviderFactory

--- database/factories/ProviderFactory.php
+++ database/factories/ProviderFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Provider::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'client_id' => function() {
+            return create(\App\Client::class)->id;
+        }
     ];
 });
